5d9f9efc271e351177aa026f37fcf80e9cc282c6
ksta11 <santicasta1003@gmail.com>
2025-02-08 21:01:31 -0500

    Se implemento una primera version de login y register

---
+++ utils/Tokens.ts
@@ -0,0 +1,5 @@
+export const standarInput = 'w-full px-4 py-3 rounded-md bg-gray-200 text-gray-700 hover:shadow-lg shadow-transition duration-500 focus:outline-secondary-200'
+
+export const standarContainer = 'p-9 rounded-lg bg-slate-100 flex flex-col'
+
+export const standarTextLink = 'hover:text-blue-600 transition-colors duration-500'
