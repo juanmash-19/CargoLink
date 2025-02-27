dbc62d8bdd64b58df07e4a73da15da330b6d751d
ksta11 <santicasta1003@gmail.com>
2025-02-27 11:51:09 -0500

    Hotfix: se integro el home

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
-import Header from "@/components/molecules/Header";
+import { Metadata } from "next";
+import ScreemHomePage from "@/modules/home/ScreenHome";
 
-export default function Home() {
-  return (
-    <main className="w-full flex gap-10 items-center">
-      
-    </main>
-  );
+export const metadata: Metadata = {
+    title: "Home",
+    description: "Bienvenido a Cargo Link",
+    alternates: {
+        canonical: 'https://mydomain.com/home'
+    }
+}
+
+export default function Home(){
+    return <ScreemHomePage />
 }
