7ab2836d92d04d76d4d2e130618c7d81fc3d3000
juanmash-19 <juanmasuhe1909@gmail.com>
2025-03-13 15:59:08 -0500

    Agregando not-found por modulos desde el principal

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -2,15 +2,41 @@
 
 
 import CustomButton from '@/components/atoms/CustomButton'
-import { useRouter } from 'next/navigation';
+import { useRouter, usePathname } from 'next/navigation';
 
+const NotFoundData = {
+    'user': {
+        redirectPath: '/',
+        message: 'Cannot access profile, path not found'
+    }
+}
+
+const getMessage = (obj, current: any[]) => {
+    if (current.length == 1) {
+        const d = [obj[current[0]]['message'], obj[current[0]]['redirectPath']]
+        return d
+    }
+    const aux = current.shift()
+    
+    return getMessage(obj[aux], current)
+}
 export default function NotFound(){
 
     
     const router = useRouter();
+    const currentPath = usePathname().split('/').filter((v) => {
+        if (v) return true;
+        return false    
+    })
 
+    let data: any[]
+    if (currentPath){
+        data = getMessage(NotFoundData, currentPath)
+    }
+    
+    
     const menuClick = () =>{
-        router.push('/')
+        router.push(data[1])
     };
 
     return (
@@ -20,7 +46,7 @@ export default function NotFound(){
                     <div className="mx-auto max-w-screen-sm text-center">
                         <h1 className="mb-4 text-7xl tracking-tight font-extrabold lg:text-9xl text-primary-600">404</h1>
                         <p className="mb-4 text-3xl tracking-tight font-bold text-gray-900 md:text-4xl">Something's missing.</p>
-                        <p className="mb-4 text-lg font-light text-gray-500 ">Sorry, we can't find that page. You'll find lots to explore on the home page. </p>
+                        <p className="mb-4 text-lg font-light text-gray-500 ">{data[0]}</p>
                             <CustomButton 
                                 text='Volver al menu'
                                 variant='primary'
