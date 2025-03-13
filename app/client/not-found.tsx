e3507894cde50bd092ec75423cb0dde1fd03c60c
juanmash-19 <juanmasuhe1909@gmail.com>
2025-03-13 09:28:50 -0500

    Dando estructura a las paginas personalizadas del not-found, falta pulir cositas

--- app/client/not-found.tsx
+++ app/client/not-found.tsx
@@ -10,7 +10,7 @@ export default function NotFound(){
     const router = useRouter();
 
     const menuClick = () =>{
-        router.push('/')
+        router.push('/home')
     };
 
     return (
@@ -20,7 +20,7 @@ export default function NotFound(){
                     <div className="mx-auto max-w-screen-sm text-center">
                         <h1 className="mb-4 text-7xl tracking-tight font-extrabold lg:text-9xl text-primary-600">404</h1>
                         <p className="mb-4 text-3xl tracking-tight font-bold text-gray-900 md:text-4xl">Something's missing.</p>
-                        <p className="mb-4 text-lg font-light text-gray-500 ">Sorry, we can't find that page. You'll find lots to explore on the home page. </p>
+                        <p className="mb-4 text-lg font-light text-gray-500 ">Sorry, the client page was not found. </p>
                             <CustomButton 
                                 text='Volver al menu'
                                 variant='primary'
